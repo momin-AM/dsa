d79d797e7f15b26cf8d804f94eadea0edf44ad7a
Momin <sk4549151@gmail.com>
2025-10-14 15:08:24 +0530

    Create complexityTS.cpp

---
+++ complexityTS.cpp
@@ -0,0 +1,18 @@
+//time complexity--------
+
+//def - extra memory space taken by program apart from input outputmemory
+//analised using asymtopic (same as time complexity)
+
+/*1.worst case memory complexity(O(n)) -:
+    constant
+    log(n)
+    sqrt(n)
+    n
+    nlog(n)
+    n*n
+    n*n*n
+    2^n
+    etc...*/
+/*best case time complexity(big omega(1))*/
+
+/*average case time complexity (big theta(n))*/
