2d7dbb4a226ce53239af9a505886a2e5cb44d829
Momin <sk4549151@gmail.com>
2025-10-15 08:44:18 +0530

    Update complexityTS.cpp

--- complexityTS.cpp
+++ complexityTS.cpp
@@ -1,4 +1,39 @@
-//time complexity--------
+//time complexity
+
+#include <iostream>
+using namespace std;
+//time complexity : number of calculations in worst case time complexity
+//same as space complexity...
+//O(n) etc etc.
+
+//an example for optimising time complexity
+//calculate sum in range of x,y
+int sumInRange(int x,int y){//simple bruteforce
+    int sum=0;
+    for(int i=x;i<=y;i++){
+        sum+=i;
+    }
+    return sum;
+}
+
+int sumInRangeOptimised(int x,int y){
+    int n=y-x+1, d=1;
+    int sum=(n)*(2*x+(n-1)*d)/2;
+    return sum;
+}
+
+int main(){
+    int x,y;
+    cin>>x>>y;
+    //int res=sumInRange(x,y);
+    int res=sumInRangeOptimised(x,y);
+    cout<<res<<endl;
+    return 0;
+}
+
+
+
+//space complexity--------
 
 //def - extra memory space taken by program apart from input outputmemory
 //analised using asymtopic (same as time complexity)
